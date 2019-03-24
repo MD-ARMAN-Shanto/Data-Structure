84bed89ae362371e4d4865fd21dbb5343ddbacd4
MD-ARMAN-Shanto <shanto377@gmail.com>
2019-03-24 14:24:29 +0600

    commit

---
+++ App.py
@@ -0,0 +1,17 @@
+from LinkedlistDS.Linkedlist import Linkedlist
+
+linkedList = Linkedlist()
+
+linkedList.insertEnd(11)
+linkedList.insertEnd(122)
+linkedList.insertEnd(3)
+linkedList.insertStart(31)
+linkedList.traverseList()
+linkedList.remove(122)
+linkedList.remove(3)
+linkedList.traverseList()
+linkedList.insertEnd(22)
+
+linkedList.traverseList()
+
+
